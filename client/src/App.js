f75d5d1ebc5368b4a6e568640c4cec278fc4463a
Jonathan Demers <jdemers@opcodesolutions.com>
2021-11-28 12:52:11 -0500

    Add React frontend

---
+++ client/src/App.js
@@ -0,0 +1,15 @@
+import "./App.css";
+
+import Navbar from "./components/layout/Navbar";
+import Landing from "./components/layout/Landing";
+
+const App = () => {
+  return (
+    <>
+      <Navbar />
+      <Landing />
+    </>
+  );
+};
+
+export default App;
