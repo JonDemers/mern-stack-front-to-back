489be8e69b8c1b9d7b18e7664063c0e392421334
Jonathan Demers <jdemers@opcodesolutions.com>
2021-11-28 13:12:30 -0500

    Add react-router-dom

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,24 @@
 import "./App.css";
-
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Navbar from "./components/layout/Navbar";
 import Landing from "./components/layout/Landing";
+import Login from "./components/auth/Login";
+import Register from "./components/auth/Register";
 
 const App = () => {
   return (
-    <>
+    <Router>
       <Navbar />
-      <Landing />
-    </>
+      <Routes>
+        <Route exact path="/" element={<Landing />} />
+      </Routes>
+      <section className="container">
+        <Routes>
+          <Route exact path="/register" element={<Register />} />
+          <Route exact path="/login" element={<Login />} />
+        </Routes>
+      </section>
+    </Router>
   );
 };
 
