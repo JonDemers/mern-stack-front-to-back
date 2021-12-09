565a35221fe90ab1b28875d4f03bb3d721dbdbd6
Jonathan Demers <jdemers@opcodesolutions.com>
2021-12-09 08:01:15 -0500

    Add deployment script

---
+++ build-and-deploy.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+set -e
+
+if [ "$(git status -s)" != "" ]; then
+  git status
+  echo
+  echo "***** You have local changes. Make sure to commit or revert them before deploying. *****"
+  exit 1
+fi
+
+rm -rf ./client/build
+npm run build --prefix client
+
+rm -rf ./docs
+mv ./client/build docs
+
+git add ./docs
+git commit -m "Deploy frontent to gh-pages"
+git push origin HEAD
+
+git push heroku HEAD
+
+echo
+echo "***** Deployment succeeded *****"
