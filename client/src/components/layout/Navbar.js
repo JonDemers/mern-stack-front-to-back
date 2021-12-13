3e45b63178e66bfdbbaae6be843a3791c938ef79
Jonathan Demers <jdemers@opcodesolutions.com>
2021-12-13 06:14:49 -0500

    Display guestLinks in Navbar by default

--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -48,7 +48,7 @@ export const Navbar = ({ auth: { isAuthenticated, loading }, logout }) => {
           <i className="fas fa-code"></i> DevConnector
         </Link>
       </h1>
-      {!loading && (isAuthenticated ? authLinks : guestLinks)}
+      {isAuthenticated ? authLinks : guestLinks}
     </nav>
   );
 };
