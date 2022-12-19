579783ec5412cedf5a6ebd445eed0988a028507d
Jonathan Demers <jdemers@opcodesolutions.com>
2022-12-19 06:52:10 -0500

    Migrate private docker registry

--- build-deploy-backend.sh
+++ build-deploy-backend.sh
@@ -4,10 +4,10 @@ docker build --no-cache --tag com.opcode.mern-stack-front-to-back .
 
 tag=latest-$(date "+%Y.%m.%d-%H.%M.%S")
 
-docker login -u $DOCKER_CREDENTIAL_USR -p $DOCKER_CREDENTIAL_PSW http://debian-cloud-test:8083
+#docker login -u $DOCKER_CREDENTIAL_USR -p $DOCKER_CREDENTIAL_PSW http://debian-cloud-test:8083
 
-docker tag com.opcode.mern-stack-front-to-back debian-cloud-test:8083/com.opcode.mern-stack-front-to-back:$tag
+docker tag com.opcode.mern-stack-front-to-back docker-registry.k8s.lan/com.opcode.mern-stack-front-to-back:$tag
 
-docker push debian-cloud-test:8083/com.opcode.mern-stack-front-to-back:$tag
+docker push docker-registry.k8s.lan/com.opcode.mern-stack-front-to-back:$tag
 
 cat k8s.yml | sed -r "s/\{\{image_version\}\}/$tag/g" | kubectl apply -f -
