40845617d20643adca51f85745bfe2fec7c299be
Jonathan Demers <jdemers@opcodesolutions.com>
2023-01-08 08:40:49 -0500

    Update docker-registry port

--- build-deploy-backend.sh
+++ build-deploy-backend.sh
@@ -6,8 +6,8 @@ tag=latest-$(date "+%Y.%m.%d-%H.%M.%S")
 
 #docker login -u $DOCKER_CREDENTIAL_USR -p $DOCKER_CREDENTIAL_PSW http://debian-cloud-test:8083
 
-docker tag com.opcode.mern-stack-front-to-back docker-registry.k8s.lan/com.opcode.mern-stack-front-to-back:$tag
+docker tag com.opcode.mern-stack-front-to-back docker-registry.k8s.lan:30501/com.opcode.mern-stack-front-to-back:$tag
 
-docker push docker-registry.k8s.lan/com.opcode.mern-stack-front-to-back:$tag
+docker push docker-registry.k8s.lan:30501/com.opcode.mern-stack-front-to-back:$tag
 
 cat k8s.yml | sed -r "s/\{\{image_version\}\}/$tag/g" | kubectl apply -f -
